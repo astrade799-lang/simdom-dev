681c6eadca0c27683ea87027c4dd0578342d9f95
NamaKamu <an.seventh7@gmail.com>
2026-07-20 23:42:23 +0800

    user-table fix error v2

--- src/app/(dashboard)/dashboard/users/_components/UserTable.tsx
+++ src/app/(dashboard)/dashboard/users/_components/UserTable.tsx
@@ -11,7 +11,7 @@ type User = {
   name: string
   namaLengkap: string | null  // ← TAMBAH
   nip: string | null          // ← TAMBAH
-  jabatan?: string | null 
+  jabatan: string | null 
   email: string
   role: Role
   skpdId: string | null
